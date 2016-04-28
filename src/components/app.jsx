654e9b00222ad6ff9ce881c6712579d724a5d04f
Jessica <jessjkim@gmail.com>
2016-04-27 20:42:06 -0400

    Add container.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,69 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var SessionPicker = require('./sessionPicker');
-var MeditateActions = require('./meditateActions');
-var Countdown = require('./countdown');
+var Container = require('./container');
 
 var App = React.createClass({
-  getInitialState: function() {
-    return {
-      longSessionCountdown: 1200,
-      shortSessionCountdown: 300,
-      originalCountdown: 300,
-      countdown: 300,
-      running: false
-    }
-  },
-
-  tick: function() {
-    if (this.state.running) {
-      this.setState({countdown: this.state.countdown - 1});
-    }
-  },
-
-  setLongSession: function() {
-    this.setState({originalCountdown: this.state.longSessionCountdown, countdown: this.state.longSessionCountdown, running: false})
-  },
-
-  setShortSession: function() {
-    this.setState({originalCountdown: this.state.shortSessionCountdown, countdown: this.state.shortSessionCountdown, running: false})
-  },
-
-  updateCountdown: function(seconds) {
-    this.setState({countdown: this.state.countdown + seconds});
-  },
-
-  start: function() {
-    if (this.state.running == false) {
-      this.setState({running: true});
-    }
-  },
-
-  pause: function() {
-    if (this.state.running == true) {
-      this.setState({running: false});
-    }
-  },
-
-  reset: function() {
-    if (this.state.running == true) {
-      this.setState({running: false})
-    }
-    this.setState({countdown: this.state.originalCountdown})
-  },
-
-  componentDidMount: function() {
-    setInterval(this.tick, 1000);
-  },
 
   render: function () {
     return(
       <div>
-        <h1>My Meditation Countdown Timer</h1>
-        <Countdown countdown={this.state.countdown} updateCountdown={this.updateCountdown} />
-        <SessionPicker setLongSession={this.setLongSession} setShortSession={this.setShortSession}/>
-        
-        <MeditateActions running={this.state.running} start={this.start} pause={this.pause} reset={this.reset} />
+        <h1>My Meditation Countdown App Timer</h1>
+        <Container />
       </div>
     );
   }
